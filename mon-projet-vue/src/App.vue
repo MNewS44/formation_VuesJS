847044a01d55a7969494b5ca0967392345571211
MNewS44 <72104062+MNewS44@users.noreply.github.com>
2024-02-01 15:38:59 +0100

    TP2 et TP3

---
+++ mon-projet-vue/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <LoginForm title="Authentification" />
+  </div>
+</template>
+
+<script>
+import LoginForm from './components/LoginForm.vue';
+
+export default {
+  name: 'App',
+  components: {
+    LoginForm
+  }
+}
+</script>
+
+<style>
+@import './assets/base.css';
+</style>
